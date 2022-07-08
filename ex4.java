8b2facfb93b36d4f6c252102b03cf6fe2431ff17
s1290078 <s1290078@u-aizu.ac.jp>
2022-07-08 17:25:23 +0900

    User name added

--- ex4.java
+++ ex4.java
@@ -5,6 +5,11 @@ public class ex4{
     public static void main(String[] args){
         Random rand = new Random();
         String[] headOrTails = {"Heads","Tails"};
+
+	System.out.println("Who are you?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello, " + name + "!");
 	
         System.out.println("Tossing a coin...");
         int countHeads = 0;
